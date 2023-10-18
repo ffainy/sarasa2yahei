e807fc1b66a3201e3c07f78d585ed5e6f2f3d036
Neo <neotpravlennoye@outlook.com>
2023-10-19 03:40:27 +0800

    1

--- exec.py
+++ exec.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
     gen_simsun_cl.gen_simsun_ext_cl()
     print('simsun cl successfully generated')
 
-    gen_yahei_sc.gen_yahei_regular_sc()
-    gen_yahei_sc.gen_yahei_bold_sc()
-    gen_yahei_sc.gen_yahei_light_sc()
-    print('yahei sc successfully generated')
+    # gen_yahei_sc.gen_yahei_regular_sc()
+    # gen_yahei_sc.gen_yahei_bold_sc()
+    # gen_yahei_sc.gen_yahei_light_sc()
+    # print('yahei sc successfully generated')
 
-    gen_simsun_sc.gen_simsun_sc()
-    gen_simsun_sc.gen_simsun_ext_sc()
-    print('simsun sc successfully generated')
+    # gen_simsun_sc.gen_simsun_sc()
+    # gen_simsun_sc.gen_simsun_ext_sc()
+    # print('simsun sc successfully generated')
 
 
